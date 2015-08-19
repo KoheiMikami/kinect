cdbffbe950ad4148a9395f0acc7e9abe64a0737b
Kohei Mikami <nyatamon@gmail.com>
2015-08-19 21:01:43 +0900

    onTheWay

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,12 +2,52 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofSetWindowShape(kinectWidth,kinectHeight);
 
+	//sensor‚ðŽæ“¾
+	HRESULT hResult = S_OK;
+	hResult = GetDefaultKinectSensor(&pSensor);
+	if (FAILED(hResult)) {
+		 std::cerr << "Error : GetDefaultKinectSensor" << std::endl;
+		//return -1;
+	}
+
+	//sensor‚ðŠJ‚­
+	hResult = pSensor->Open();
+	if( FAILED( hResult ) ){
+		std::cerr << "Error : IKinectSensor::Open()" << std::endl;
+		//return -1;
+	}
+
+	//source‚ðŽæ“¾
+	hResult = pSensor->get_ColorFrameSource( &pColorSource ); 
+		if( FAILED( hResult ) ){
+		std::cerr << "Error : IKinectSensor::get_ColorFrameSource()" << std::endl;
+		//return -1;
+	}
+
+	//Reader‚ðŽæ“¾Areader‚ðŠJ‚­
+	hResult = pColorSource->OpenReader( &pColorReader );
+	if( FAILED( hResult ) ){
+		std::cerr << "Error : IColorFrameSource::OpenReader()" << std::endl;
+		//return -1;
+	}
+
+	hResult = pColorSource->CreateFrameDescription(ColorImageFormat::ColorImageFormat_Rgba, &colorDescription );
+	if(FAILED(hResult)){
+		std::cerr << "Error : IColorFrameSource::get_FrameDescription()" << std::endl;
+		//return false;
+	}
+	colorDescription->get_Width( &colorWidth );
+	colorDescription->get_Height( &colorHeight );
+	colorDescription->get_BytesPerPixel( &colorBytesPerPixels);
+
+	colorImage.allocate(kinectWidth,kinectHeight,OF_IMAGE_COLOR_ALPHA);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+	
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -5,6 +5,9 @@
 #include <kinect.h>
 #include <Windows.h>
 
+const int kinectWidth = 1920;
+const int kinectHeight = 1080;
+
 class ofApp : public ofBaseApp{
 
 	public:
@@ -21,5 +24,19 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+
+		//Sencer
+		IKinectSensor *pSensor;
+
+		//Source
+		IColorFrameSource *pColorSource;
+
+		//Reader
+		IColorFrameReader *pColorReader;
 		
+		ofImage colorImage;
+
+		IFrameDescription* colorDescription;
+		int colorWidth, colorHeight;
+		unsigned int colorBytesPerPixels;
 };
